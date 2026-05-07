a7c97cc6049d8646dc68d77c641e0ea1a3c17da4
Shota Kudo (chaploud) <shota.508@studist.jp>
2026-05-07 13:22:23 +0900

    Revert "diag(p7): print unsupported op name on x86_64 emit reject (chunk 14b probe)"
    This reverts commit 2d559ab2005e093a4531535c6e252277074da1dc.

--- src/engine/codegen/x86_64/emit.zig
+++ src/engine/codegen/x86_64/emit.zig
@@ -691,10 +691,7 @@ pub fn compile(
                 }
                 break;
             },
-            else => {
-                std.debug.print("x86_64/emit: unsupported op `{s}` (func_idx={d})\n", .{ @tagName(ins.op), func.func_idx });
-                return Error.UnsupportedOp;
-            },
+            else => return Error.UnsupportedOp,
         }
     }
 
